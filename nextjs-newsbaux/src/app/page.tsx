332a545641fbb1d06295fd5cc939086fe6aa95ad
Jack <jackmu@umich.edu>
2025-10-01 21:30:09 -0700

    setup auth

--- nextjs-newsbaux/src/app/page.tsx
+++ nextjs-newsbaux/src/app/page.tsx
@@ -1,6 +1,18 @@
-export default function Home() {
+import { signIn, auth } from "@/auth"
+import { Session } from "next-auth";
+
+export default async function Home() {
+	const session: Session | null = await auth();
+	console.log(session);
+
 	return (
 		<div>
+			<form action={async () => {
+				"use server";
+				await signIn("github");
+			}}>
+				<button type="submit">Create</button>
+			</form>
 
 		</div>
 	);
